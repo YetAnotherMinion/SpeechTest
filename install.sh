312bdf770cba1310d955ed16bebd8db5539ae38c
YetAnotherMinion <yetanotherminion@gmail.com>
2015-10-30 19:34:13 -0400

    now that build passes, pipe remaining tests to dev null

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@
 # configured in travis
 # SPHINX_ROOT=${SPHINX_ROOT:-$(pwd)}
 
-cd sphinxbase/
+cd sphinxbase/k
 mkdir linux-default-build
 BASE_INSTALL_LOCATION=$SPHINX_ROOT/sphinxbase/linux-default-build
-sudo ./autogen.sh #this triggers the problem with the ltmain
+sudo ./autogen.sh </dev/null >/dev/null 2>&1 #this triggers the problem with the ltmain
 sudo ./autogen.sh --prefix=$BASE_INSTALL_LOCATION #this runs libtoolize a second time which now correctly copies the ltmain.sh
 
 sudo make </dev/null >/dev/null 2>&1
@@ -23,11 +23,11 @@ cd ../pocketsphinx
 mkdir $SPHINX_ROOT/pocketsphinx/linux-default-build
 export SPHINXBASE_LIBS=$BASE_INSTALL_LOCATION
 POCKET_INSTALL_LOCATION=$SPHINX_ROOT/pocketsphinx/linux-default-build
-sudo ./autogen.sh #this triggers the problem with the ltmain
+sudo ./autogen.sh </dev/null >/dev/null 2>&1 #this triggers the problem with the ltmain
 sudo ./autogen.sh --prefix=$POCKET_INSTALL_LOCATION #this runs libtoolize a second time which now correctly copies the ltmain.sh
 
-sudo make #build it first
-sudo make install
+sudo make </dev/null >/dev/null 2>&1 #build it first
+sudo make install </dev/null >/dev/null 2>&1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$POCKET_INSTALL_LOCATION/lib
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$POCKET_INSTALL_LOCATION/lib/pkgconfig
 
@@ -36,11 +36,9 @@ echo $LD_LIBRARY_PATH
 echo $PKG_CONFIG_PATH
 echo "========================="
 
-sudo make check
+sudo make check </dev/null >/dev/null 2>&1
 #check the package config
 pkg-config --cflags --libs pocketsphinx sphinxbase
-
-
 
 # #return to base
 # cd ../
