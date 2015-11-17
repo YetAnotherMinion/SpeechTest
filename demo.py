150a8237bbabf1bbd5546d6476fbe810a8261ac9
YetAnotherMinion <yetanotherminion@gmail.com>
2015-11-16 23:15:08 -0500

    refactored into function and helper result object

--- demo.py
+++ demo.py
@@ -39,46 +39,64 @@ from os import environ, path
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
-MODELDIR = "pocketsphinx/model"
-DATADIR = "pocketsphinx/test/data"
+class CandiateUtterance(object):
+    def __init__(self):
+        self.hypothesis = None
+        self.hypothesis_segments = []
+        self.nbest = []
 
-# Create a decoder with certain model
-config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
 
-# Decode streaming data.
-decoder = Decoder(config)
 
-print ("Pronunciation for word 'hello' is ", decoder.lookup_word("hello"))
-print ("Pronunciation for word 'abcdf' is ", decoder.lookup_word("abcdf"))
+def decode_from_file(filename):
+    MODELDIR = "pocketsphinx/model"
+    DATADIR = "pocketsphinx/test/data"
 
-decoder.start_utt()
-stream = open(path.join("res/", 'gripper_open.raw'), 'rb')
-while True:
-  buf = stream.read(1024)
-  if buf:
-    decoder.process_raw(buf, False, False)
-  else:
-    break
-decoder.end_utt()
+    # Create a decoder with certain model
+    config = Decoder.default_config()
+    config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+    config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+    config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
 
-hypothesis = decoder.hyp()
-print ('Best hypothesis: ', hypothesis.hypstr, " model score: ", hypothesis.best_score, " confidence: ", hypothesis.prob)
+    # Decode streaming data.
+    decoder = Decoder(config)
 
-print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+    # print ("Pronunciation for word 'hello' is ", decoder.lookup_word("hello"))
+    # print ("Pronunciation for word 'abcdf' is ", decoder.lookup_word("abcdf"))
+
+    decoder.start_utt()
+    stream = open(filename, 'rb')
+    while True:
+        buf = stream.read(1024)
+        if buf:
+            decoder.process_raw(buf, False, False)
+        else:
+            break
+    decoder.end_utt()
+
+    cu = CandiateUtterance()
+    cu.hypothesis = decoder.hyp()
+    cu.hypothesis_segments = [seg.word for seg in decoder.seg()]
+    cu.nbest = zip(range(10), decoder.nbest())
+
+    return cu
+
+fn = path.join('res', 'goforward.raw')
+candidate = decode_from_file(fn)
+
+print ('Best hypothesis: ', candidate.hypothesis.hypstr, " model score: ", candidate.hypothesis.best_score, " confidence: ", candidate.hypothesis.prob)
+
+print ('Best hypothesis segments: ', candidate.hypothesis_segments)
 
 # Access N best decodings.
 print ('Best 10 hypothesis: ')
-for best, i in zip(decoder.nbest(), range(10)):
+for i, best in candidate.nbest:
     print (best.hypstr, best.score)
 
-stream = open(path.join(DATADIR, 'goforward.mfc'), 'rb')
-stream.read(4)
-buf = stream.read(13780)
-decoder.start_utt()
-decoder.process_cep(buf, False, True)
-decoder.end_utt()
-hypothesis = decoder.hyp()
-print ('Best hypothesis: ', hypothesis.hypstr, " model score: ", hypothesis.best_score, " confidence: ", hypothesis.prob)
+# stream = open(path.join(DATADIR, 'goforward.mfc'), 'rb')
+# stream.read(4)
+# buf = stream.read(13780)
+# decoder.start_utt()
+# decoder.process_cep(buf, False, True)
+# decoder.end_utt()
+# hypothesis = decoder.hyp()
+# print ('Best hypothesis: ', hypothesis.hypstr, " model score: ", hypothesis.best_score, " confidence: ", hypothesis.prob)
