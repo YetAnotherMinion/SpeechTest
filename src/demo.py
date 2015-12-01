e4f4e4482a8e693a0a961ce0302a66d5526c5025
YetAnotherMinion <yetanotherminion@gmail.com>
2015-12-01 15:13:02 -0500

    refactored out the stdout and stderr surpression class

--- src/demo.py
+++ src/demo.py
@@ -83,6 +83,47 @@ def decode_from_file(filename, decoder = None):
     cu.nbest = zip(range(10), decoder.nbest())
     return cu
 
+def loop_decode(seconds = 3, decoder = None):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    RECORD_SECONDS = seconds
+    try:
+        while True:
+            fn = "o"+str(x)+".wav"
+            p = pyaudio.PyAudio()
+            stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+            print("* recording")
+            frames = []
+            n_frames = int(RATE / CHUNK * RECORD_SECONDS)
+            for i in range(0, n_frames):
+                print i
+                data = stream.read(CHUNK)
+                frames.append(data)
+            print("* done recording")
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+            
+            decoder.start_utt()
+            for buf in frames:
+                if buf:
+                    decoder.process_raw(buf, False, False)
+                else:
+                    break
+            decoder.end_utt()
+
+            cu = CandiateUtterance()
+            cu.hypothesis = decoder.hyp()
+            cu.hypothesis_segments = [seg.word for seg in decoder.seg()]
+            cu.nbest = zip(range(10), decoder.nbest())
+            yield cu
+
+    except GeneratorExit:
+        #finished
+        pass
+
 # stream = open(path.join(DATADIR, 'goforward.mfc'), 'rb')
 # stream.read(4)
 # buf = stream.read(13780)
--- tests/single_word_unittest.py
+++ tests/single_word_unittest.py
@@ -16,54 +16,26 @@ if G_PROJECT_ROOT_DIR not in sys.path:
      sys.path.insert(0, G_PROJECT_ROOT_DIR)
 
 from src.demo import decode_from_file
+from src.util import suppress_stdout_stderr
 
 # Define a context manager to suppress stdout and stderr.
-class suppress_stdout_stderr(object):
-    '''
-    A context manager for doing a "deep suppression" of stdout and stderr in 
-    Python, i.e. will suppress all print, even if the print originates in a 
-    compiled C/Fortran sub-function.
-       This will not suppress raised exceptions, since exceptions are printed
-    to stderr just before a script exits, and after the context manager has
-    exited (at least, I think that is why it lets exceptions through).      
-
-    '''
-    def __init__(self):
-        # Open a pair of null files
-        self.null_fds =  [os.open(os.devnull,os.O_RDWR) for x in range(2)]
-        # Save the actual stdout (1) and stderr (2) file descriptors.
-        self.save_fds = (os.dup(1), os.dup(2))
-
-    def __enter__(self):
-        # Assign the null pointers to stdout and stderr.
-        os.dup2(self.null_fds[0],1)
-        os.dup2(self.null_fds[1],2)
-
-    def __exit__(self, *_):
-        # Re-assign the real stdout/stderr back to (1) and (2)
-        os.dup2(self.save_fds[0],1)
-        os.dup2(self.save_fds[1],2)
-        # Close the null files
-        os.close(self.save_fds[0])
-        os.close(self.save_fds[1])
-        os.close(self.null_fds[0])
-        os.close(self.null_fds[1])
-
 
 class TestSingleWord(unittest.TestCase):
     def setUp(self):
-        pass
-    # def test_goforward(self):
-    #     with suppress_stdout_stderr():
-    #         fn = os.path.join(G_PROJECT_ROOT_DIR, 'res', 'goforward.raw')
-    #         x = decode_from_file(fn)
-    #     print ('Best hypothesis: ', x.hypothesis.hypstr, " model score: ", x.hypothesis.best_score, " confidence: ", x.hypothesis.prob)
-    #     print ('Best hypothesis segments: ', x.hypothesis_segments)
+        MODELDIR = "pocketsphinx/model"
+        DATADIR = "pocketsphinx/test/data"
 
-    #     # Access N best decodings.
-    #     print ('Best 10 hypothesis: ')
-    #     for i, best in x.nbest:
-    #         print (best.hypstr, best.score)
+        # Create a decoder with certain model
+        config = Decoder.default_config()
+        config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
+        config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+        config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+         # Decode streaming data.
+        try:
+            self.decoder = Decoder(config)
+        except RuntimeError:
+            time.sleep(1) # try waiting and trying again
+            self.decoder = Decoder(config)
 
     def test_single_words(self):
         in_file = os.path.join(G_TEST_DIR, 'command_words.yml')
@@ -86,22 +58,6 @@ class TestSingleWord(unittest.TestCase):
         word_score = {}
         count = 0
 
-        MODELDIR = "pocketsphinx/model"
-        DATADIR = "pocketsphinx/test/data"
-
-        # Create a decoder with certain model
-        config = Decoder.default_config()
-        config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
-        config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-        config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
-
-        # Decode streaming data.
-        try:
-            decoder = Decoder(config)
-        except RuntimeError:
-            time.sleep(1) # try waiting and trying again
-            decoder = Decoder(config)
-
         for tc in flatten(testcases):
             sample_word = tc[-1]
             count += 1
@@ -112,7 +68,7 @@ class TestSingleWord(unittest.TestCase):
             fn = os.path.join(G_PROJECT_ROOT_DIR, *tc[0:-1])
             print fn
             with suppress_stdout_stderr():
-                x = decode_from_file(fn, decoder)
+                x = decode_from_file(fn, self.decoder)
 
             if x.hypothesis.hypstr == sample_word:
                 word_score[sample_word]["correct"] += 1
